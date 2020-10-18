580773fa2deabeef85b21571c8d2ae7d0c12abed
Rucha Tambe <rtambe@usc.edu>
2020-10-18 10:11:55 -0700

    Made few changes in files

---
+++ AR measuring distance/Assets/Scripts/ScreenDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScreenDisplay : MonoBehaviour
+{
+    public Text display;
+    private string s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        display=GetComponent<Text>();
+
+        s="Hello"; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        display=GetComponent<Text>();
+        s=s+"In update";
+        display.text=s;
+
+    }
+}
