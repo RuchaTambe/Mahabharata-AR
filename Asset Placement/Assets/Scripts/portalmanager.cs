3ba715307feb365e72a0a6037fc5a83d03cb253d
Ankita Agrawal <ankitaagrawal@Ankitas-MacBook-Air.local>
2020-10-17 23:14:23 -0700

    jewels added

---
+++ Asset Placement/Assets/Scripts/portalmanager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+public class portalmanager : MonoBehaviour
+{
+    public GameObject InnerWorld;
+    //This materials matter needs to be optimizated!
+    public Material[] materials;
+    private Vector3 camPostionInPortalSpace;
+    bool wasInFront;
+    bool inOtherWorld;
+    bool hasCollided;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMaterials(false);
+    }
+    void SetMaterials(bool fullRender)
+    {
+        var stencilTest = fullRender ? CompareFunction.NotEqual : CompareFunction.Equal;
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilComp", (int)stencilTest);
+        }
+    }
+    //Set bidirectional function
+    bool GetIsInFront()
+    {
+        GameObject MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        Vector3 worldPos = MainCamera.transform.position + MainCamera.transform.forward * Camera.main.nearClipPlane;
+        camPostionInPortalSpace = transform.InverseTransformPoint(worldPos);
+        return camPostionInPortalSpace.y >= 0 ? true : false;
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        GameObject MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        if (collider.transform != MainCamera.transform)
+            return;
+        wasInFront = GetIsInFront();
+        hasCollided = true;
+    }
+    // Update is called once per frame
+    void OnTriggerExit(Collider collider)
+    {
+        GameObject MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        if (collider.transform != MainCamera.transform)
+            return;
+        hasCollided = false;
+    }
+    void whileCameraColliding()
+    {
+        if (!hasCollided)
+            return;
+        bool isInFront = GetIsInFront();
+        // if (isInFront && !wasInFront || wasInFront && !isInFront)
+        //  if (isInFront && !wasInFront)
+        // {
+        //     inOtherWorld = !inOtherWorld;
+        //     SetMaterials(inOtherWorld);
+        // }
+        wasInFront = isInFront;
+    }
+    private void OnDestroy()
+    {
+        SetMaterials(true);
+    }
+    private void Update()
+    {
+        whileCameraColliding();
+    }
+}
+
